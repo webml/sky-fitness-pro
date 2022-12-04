2483b24c3b933761ad7e335f7bf33cdeba694a9c
Allgud <allgud83@gmail.com>
2022-12-04 12:53:10 +0300

    added modal change-login&password

---
+++ src/components/Modals/change-login&password/styles.js
@@ -0,0 +1,16 @@
+import styled from "styled-components";
+
+export const ModalTitle = styled.h4`
+    width: 100%;
+    font-family: StratosSkyeng, sans-serif;
+    font-style: normal;
+    font-weight: 500;
+    font-size: 18px;
+    line-height: 24px;
+    letter-spacing: -0.05px;
+    margin-top: 10px;
+`
+
+export const ModalButtonBox = styled.div`
+    margin-top: 20px;
+`
